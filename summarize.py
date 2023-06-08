21b62a224fd2ebbd9bc4b3866cda385aefe3ed4e
Tony Bricca <tonybricca@tonys-air.lan>
2023-06-08 09:01:12 -0700

    first commit

--- summarize.py
+++ summarize.py
@@ -6,6 +6,7 @@ input_text = input("Enter text to be summarized: ")
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages= [
+        # this gives the api a role 
         {"role": "system", "content": "You are a text summarizer chat bot. Your goal is to summarize the text that is given to you by the user"},
         {"role": "user", "content" : input_text},
     ],
